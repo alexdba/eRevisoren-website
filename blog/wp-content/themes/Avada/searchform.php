90ba176cd633118e14a0a9670667ee3ace33b704
xunlight <xelo.cz@gmail.com>
2015-07-05 21:50:56 +0200

    Remove of unnecessary files

--- blog/wp-content/themes/Avada/searchform.php
+++
@@ -1,5 +0,0 @@
-<form class="search searchform" action="<?php echo home_url(); ?>/" method="get">
-	<fieldset>
-		<span class="text"><input name="s" class="s" type="text" value="" placeholder="<?php echo __('Search ...', 'Avada'); ?>" /></span>
-	</fieldset>
-</form>
