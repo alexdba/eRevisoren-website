e196495b0160d6d2c143bb4dfa65cd6cf64368c0
xunlight <xelo.cz@gmail.com>
2015-08-06 04:23:24 +0200

    New goal for google analytics - open sign up.

--- frontend/views/site/pricing-plans.php
+++ frontend/views/site/pricing-plans.php
@@ -3,7 +3,7 @@
 use yii\helpers\Url;
 use yii\helpers\Html;
 
-$this->title = 'eBogholderen.dk - Which pricing plan is right for you?';
+$this->title = 'eBogholderen.dk - Hvilken Pakke er rigtig for mig?';
 
 
 $this->registerJsFile(
