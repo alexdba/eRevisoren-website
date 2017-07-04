61ba3429c4ccf6f8153e3e7cd4cb20ab1ea8f5b8
alexdba <alexdba@gmail.com>
2017-07-04 14:53:37 -0300

    New Facebook Pixel Code

--- frontend/config/facebook-pixel-code.php
+++ frontend/config/facebook-pixel-code.php
@@ -4,11 +4,12 @@
 n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
 n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
 t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
-document,'script','//connect.facebook.net/en_US/fbevents.js');
-
-fbq('init', '1541273339523718');
-fbq('track', "PageView");</script>
+document,'script','https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '331184807318905'); // Insert your pixel ID here.
+fbq('track', 'PageView');
+</script>
 <noscript><img height="1" width="1" style="display:none"
-src="https://www.facebook.com/tr?id=1541273339523718&ev=PageView&noscript=1"
+src="https://www.facebook.com/tr?id=331184807318905&ev=PageView&noscript=1"
 /></noscript>
+<!-- DO NOT MODIFY -->
 <!-- End Facebook Pixel Code -->
